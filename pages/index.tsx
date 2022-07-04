54fe0388dc056fdb6e2c957ba499353701da3722
halisycel <isaycel2001@gmail.com>
2022-07-04 17:28:46 +0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NextPage} from 'next';
+
+const Home: NextPage = () => {
+	return (
+		<div className={'bg-red-500'}>
+          	deneme
+		</div>
+	);
+};
+
+export default Home;
