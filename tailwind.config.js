39e45d638941f7d0f8e9536399a6dfd32ef01c1e
halisycel <isaycel2001@gmail.com>
2022-07-12 19:16:44 +0300

    some configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'source-sans': ['Source Sans Pro', 'sans-serif'],
+			},
+			boxShadow: {
+				border: '0 0 0 5px rgba(59, 130, 246, 0.25)',
+			},
+		},
+	},
+	plugins: [],
+};
