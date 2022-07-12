a871eb770ae6b15cf83a213791206701dd3108ab
halisycel <isaycel2001@gmail.com>
2022-07-12 20:06:31 +0300

    add home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import {NextPage} from 'next';
+import { NextPage } from 'next';
 
 const Home: NextPage = () => {
-	return (
-		<div className={'bg-red-500'}>
-          	deneme
-		</div>
-	);
+	return <div className={'bg-red-500'}>deneme</div>;
 };
 
 export default Home;
